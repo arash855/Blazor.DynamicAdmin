2b05a0b4f903781f9eab07f911f959f8cb8636ca
arash855 <malekarash855@gmail.com>
2026-06-19 19:02:23 +0330

    Add IAdminDataProvider interface

---
+++ src/Blazor.DynamicAdmin.Abstractions/IAdminDataProvider.cs
@@ -0,0 +1,13 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blazor.DynamicAdmin.Abstractions;
+
+public interface IAdminDataProvider
+{
+    Task<AdminQueryResult> QueryAsync(AdminQueryContext context, CancellationToken cancellationToken = default);
+    Task<object?> GetAsync(AdminGetContext context, CancellationToken cancellationToken = default);
+    Task<object> CreateAsync(AdminCreateContext context, CancellationToken cancellationToken = default);
+    Task<object> UpdateAsync(AdminUpdateContext context, CancellationToken cancellationToken = default);
+    Task DeleteAsync(AdminDeleteContext context, CancellationToken cancellationToken = default);
+}
